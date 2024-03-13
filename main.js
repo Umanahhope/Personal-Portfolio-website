aed4b6be6cee455560727e8ced73e3ca8e3046e5
Hope400Augustin <109266806+Hope400Augustin@users.noreply.github.com>
2024-03-13 15:46:08 -0700

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new typed(".text"{
- Strings:["ForntEnd Developer" , "YouTuber", "Web Developer"],
+ Strings:["FrontEnd Developer" , "YouTuber", "Web Developer"],
  typeSpeed:100,
  backSpeed:100,
  backDelay:1000,
